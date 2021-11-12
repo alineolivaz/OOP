6be1fb4c5f7d147eabc15c3fd430889a4693435a
Aline <40899389+alineolivaz@users.noreply.github.com>
2021-11-12 15:39:47 -0300

    Add files via upload

---
+++ Relatorio/Relatorio/BLL/BLL_Relatorios.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BLL
+{
+    class BLL_Relatorios
+    {
+        Conexao bd = new Conexao("redeconservadora");
+        
+
+        public DataTable ListarOSporsindico()
+        {
+            try
+            {
+                return bd.ExecutarConsulta("SELECT * FROM tbl_os left join tbl_sindico on tbl_os.tbl_sindico_id_sindico = tbl_sindico.id_sindico");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataTable ListarOSporsindico(DateTime datainicio, DateTime datafim)
+        {
+            try
+            {
+                return bd.ExecutarConsulta("SELECT * FROM tbl_os left join tbl_sindico " +
+                    "on tbl_os.tbl_sindico_id_sindico = tbl_sindico.id_sindico " +
+                    "where dataabertura_os between '" + datainicio.ToString("yyyy/MM/dd") + "' and '" + datafim.ToString("yyyy/MM/dd") + "'");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public DataTable Listarservicos()
+        {
+            try
+            {
+                return bd.ExecutarConsulta("SELECT * FROM tbl_servicos");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public DataTable ListarOSporsindico(int id_servico)
+        {
+            try
+            {
+                return bd.ExecutarConsulta("SELECT * FROM tbl_os left join tbl_sindico " +
+                    "on tbl_os.tbl_sindico_id_sindico = tbl_sindico.id_sindico " +
+                    "where 	tbl_servicos_id_servico = " + id_servico);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public DataTable ListarRelatorioOS(string condicao)
+        {
+            try
+            {
+                if (condicao == "")
+                    return bd.ExecutarConsulta("SELECT * FROM tbl_os left join tbl_sindico " +
+                    "on tbl_os.tbl_sindico_id_sindico = tbl_sindico.id_sindico ");
+                else
+                    return bd.ExecutarConsulta("SELECT * FROM tbl_os left join tbl_sindico " +
+                    "on tbl_os.tbl_sindico_id_sindico = tbl_sindico.id_sindico " +
+                    "where " + condicao	);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ Relatorio/Relatorio/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Relatorio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 116);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(721, 317);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "id_os";
+            this.Column1.HeaderText = "numero da os";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "nome_sindico";
+            this.Column2.HeaderText = "nome sindico";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "dataabertura_os";
+            this.Column3.HeaderText = "data Abertura";
+            this.Column3.Name = "Column3";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(53, 13);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(53, 50);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(395, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(229, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(665, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 445);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
